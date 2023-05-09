a3433333b225e564a9c52a994d4a48d9450cae51
Frikk Jarl <Frikk@jarl.email>
2023-05-09 10:00:55 +0200

    Adding "Grid" to GridCol and GridRow

--- src/components/grid-column.component/grid-column.component.tsx
+++ src/components/grid-column.component/grid-column.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IBaseComponentProps } from '../../types';
 
 const GridCol: React.FC<IBaseComponentProps> = ({ children, className, ...rest }) => {
-    const combinedClassName = `grid-col ${className || ''}`;
+    const combinedClassName = `grid grid-col ${className || ''}`;
 
     return (
         <div className={combinedClassName.trim()} {...rest}>
