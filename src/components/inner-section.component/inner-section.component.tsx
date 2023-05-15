7c8416ee01c625d6044ef43dcf811ba3171697a3
Frikk Jarl <Frikk@jarl.email>
2023-05-15 16:26:32 +0200

    w-full & container in InnerSection

--- src/components/inner-section.component/inner-section.component.tsx
+++ src/components/inner-section.component/inner-section.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IBaseComponentProps } from '../../types';
 
 const InnerSection: React.FC<IBaseComponentProps> = ({ children, className, ...rest }) => {
-    const combinedClassName = `group ${className || ''}`;
+    const combinedClassName = `group w-full container ${className || ''}`;
 
     return (
         <div className={combinedClassName.trim()} {...rest}>
